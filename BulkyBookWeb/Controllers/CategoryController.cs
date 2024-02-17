24e622d0b5cf127df3bdf9a041f3035467dc4c22
janthony25 <junanthony.business@gmail.com>
2024-02-17 17:01:54 +1300

    Connect Add Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -18,5 +18,12 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.categories;
             return View(objCategoryList);
         }
+
+        // GET
+        public IActionResult Create()
+        {
+           
+            return View();
+        }
     }
 }
