cdbf99500300f4e4cb3130ba79f3bbaeaf156a2a
janthony25 <junanthony.business@gmail.com>
2024-02-17 17:44:57 +1300

    Server Side Validation

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -25,5 +25,20 @@ namespace BulkyBookWeb.Controllers
            
             return View();
         }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
     }
 }
